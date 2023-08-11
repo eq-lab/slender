8bba806343ce79174bfac386db3ab801145302d9
rudewalt <rudewalt@gmail.com>
2023-08-11 10:58:37 +0300

    set resources experiments

--- integration-tests/tests/pool/borrower.spec.ts
+++ integration-tests/tests/pool/borrower.spec.ts
@@ -1,46 +1,70 @@
 import { SorobanClient } from "../soroban.client";
-import { balanceOf, init, mintUnderlyingTo, registerAccount } from "../pool.sut";
+import {
+  balanceOf,
+  init,
+  mintUnderlyingTo,
+  registerAccount,
+} from "../pool.sut";
 import { lender1Keys } from "../soroban.config";
 
 describe("LendingPool", function () {
-    let client: SorobanClient;
+  let client: SorobanClient;
 
-    before(async function () {
-        client = new SorobanClient();
-        await init(client);
-    });
+  before(async function () {
+    client = new SorobanClient();
+    await init(client);
+  });
 
-    it("should TBD", async function () {
-        let lender1 = await registerAccount(client, "LENDER_1", lender1Keys);
-        let lender1Address = lender1.publicKey();
+  it("should TBD", async function () {
+    try {
+      await registerAccount(client, "LENDER_1", lender1Keys);
+    } catch {}
+    let lender1Address = lender1Keys.publicKey();
 
-        await mintUnderlyingTo(client, "XLM", lender1Address, 100_000_000_000n);
-        await mintUnderlyingTo(client, "XRP", lender1Address, 100_000_000_000n);
-        await mintUnderlyingTo(client, "USDC", lender1Address, 100_000_000_000n);
-        let lender1XlmBalance = await balanceOf(client, lender1, lender1Address, "XLM");
-        let lender1XrpBalance = await balanceOf(client, lender1, lender1Address, "XRP");
-        let lender1UsdcBalance = await balanceOf(client, lender1, lender1Address, "USDC");
+    await mintUnderlyingTo(client, "XLM", lender1Address, 100_000_000_000n);
+    await mintUnderlyingTo(client, "XRP", lender1Address, 100_000_000_000n);
+    await mintUnderlyingTo(client, "USDC", lender1Address, 100_000_000_000n);
+    let lender1XlmBalance = await balanceOf(
+      client,
+      lender1Keys,
+      lender1Address,
+      "XLM"
+    );
+    let lender1XrpBalance = await balanceOf(
+      client,
+      lender1Keys,
+      lender1Address,
+      "XRP"
+    );
 
-        //     const debtTokenResult = await this.client.sendTransaction(
-        //         process.env.TOKEN,
-        //         "mint",
-        //         tokenKeys,
-        //         addressToScVal(process.env.USER_PUBLIC),
-        //         i128ToScVal(BigInt(10000000000n))
-        //     );
+    client.setUnlimitedResources();
+    let lender1UsdcBalance = await balanceOf(
+      client,
+      lender1Keys,
+      lender1Address,
+      "USDC"
+    );
 
-        //     expect(debtTokenResult.status).to.equal("SUCCESS");
+    //     const debtTokenResult = await this.client.sendTransaction(
+    //         process.env.TOKEN,
+    //         "mint",
+    //         tokenKeys,
+    //         addressToScVal(process.env.USER_PUBLIC),
+    //         i128ToScVal(BigInt(10000000000n))
+    //     );
 
-        //     const userBalanceResult = await this.client.sendTransaction(
-        //         process.env.TOKEN,
-        //         "balance",
-        //         tokenKeys,
-        //         addressToScVal(process.env.USER_PUBLIC)
-        //     );
+    //     expect(debtTokenResult.status).to.equal("SUCCESS");
 
-        //     const minted = parseScVal(userBalanceResult.resultXdr);
+    //     const userBalanceResult = await this.client.sendTransaction(
+    //         process.env.TOKEN,
+    //         "balance",
+    //         tokenKeys,
+    //         addressToScVal(process.env.USER_PUBLIC)
+    //     );
 
-        //     expect(userBalanceResult.status).to.equal("SUCCESS");
-        //     expect(minted).to.equal(10000000000n);
-    });
+    //     const minted = parseScVal(userBalanceResult.resultXdr);
+
+    //     expect(userBalanceResult.status).to.equal("SUCCESS");
+    //     expect(minted).to.equal(10000000000n);
+  });
 });
--- integration-tests/tests/soroban.client.ts
+++ integration-tests/tests/soroban.client.ts
@@ -1,56 +1,142 @@
-import { Server, Contract, TimeoutInfinite, TransactionBuilder, Keypair, xdr, SorobanRpc, Account } from "soroban-client";
+import {
+  Server,
+  Contract,
+  TimeoutInfinite,
+  TransactionBuilder,
+  Keypair,
+  xdr,
+  SorobanRpc,
+  Account,
+} from "soroban-client";
 import { promisify } from "util";
 import "./soroban.config";
+import { Durability } from "soroban-client/lib/server";
 
 export class SorobanClient {
-    client: Server;
+  client: Server;
+  unlimitedResources: boolean;
 
-    constructor() {
-        this.client = new Server(process.env.SOROBAN_RPC_URL, {
-            allowHttp: true
-        });
-        this.client.getHealth();
+  constructor() {
+    this.client = new Server(process.env.SOROBAN_RPC_URL, {
+      allowHttp: true,
+    });
+    this.client.getHealth();
+  }
+
+  setUnlimitedResources() {
+    this.unlimitedResources = true;
+  }
+
+  async registerAccount(publicKey: string): Promise<Account> {
+    return await this.client.requestAirdrop(
+      publicKey,
+      process.env.FRIENDBOT_URL
+    );
+  }
+
+  async sendTransaction(
+    contractId: string,
+    method: string,
+    signer: Keypair,
+    ...args: xdr.ScVal[]
+  ): Promise<SorobanRpc.GetTransactionResponse> {
+    const source = await this.client.getAccount(signer.publicKey());
+    const contract = new Contract(contractId);
+
+    const contractData = await this.client.getContractData(
+      contract,
+      xdr.ScVal.scvLedgerKeyContractInstance(),
+      Durability.Persistent
+    );
+
+    const entry = xdr.LedgerEntryData.fromXDR(contractData.xdr, "base64");
+    const instance = new xdr.ScContractInstance({
+      executable: xdr.ContractExecutable.contractExecutableWasm(
+        (entry.contractData().body().value() as any).toXDR()
+      ),
+      storage: null,
+    });
+    const executable = xdr.ContractExecutable.contractExecutableWasm(
+      instance.executable().wasmHash()
+    );
+    const hash = executable.wasmHash();
+
+    const txBuilder = new TransactionBuilder(source, {
+      fee: "100",
+      networkPassphrase: process.env.PASSPHRASE,
+    })
+      .addOperation(contract.call(method, ...(args || [])))
+      .setTimeout(TimeoutInfinite);
+
+    if (this.unlimitedResources) {
+      const bodyType = xdr.ContractEntryBodyType.dataEntry();
+      const durability = xdr.ContractDataDurability.persistent();
+
+      const transactionData = new xdr.SorobanTransactionData({
+        ext: new xdr.ExtensionPoint(),
+        resources: new xdr.SorobanResources({
+          footprint: new xdr.LedgerFootprint({
+            readOnly: [
+              xdr.LedgerKey.contractData(
+                new xdr.LedgerKeyContractData({
+                  contract: contract.address().toScAddress(),
+                  key: xdr.ScVal.scvLedgerKeyContractInstance(),
+                  durability,
+                  bodyType,
+                })
+              ),
+              xdr.LedgerKey.contractCode(
+                new xdr.LedgerKeyContractCode({
+                  hash,
+                  bodyType,
+                })
+              ),
+            ],
+            readWrite: [
+              xdr.LedgerKey.contractData(
+                new xdr.LedgerKeyContractData({
+                  contract: contract.address().toScAddress(),
+                  key: xdr.ScVal.scvVec([
+                    xdr.ScVal.scvSymbol("Balance"),
+                    args[0],
+                  ]),
+                  durability,
+                  bodyType,
+                })
+              ),
+            ],
+          }),
+          instructions: 100_000_000,
+          readBytes: 100_000_000,
+          writeBytes: 100_000_000,
+          extendedMetaDataSizeBytes: 204_800,
+        }),
+        refundableFee: xdr.Int64.fromString("204800"),
+      });
+
+      txBuilder.setSorobanData(transactionData);
     }
 
-    async registerAccount(publicKey: string): Promise<Account> {
-        return await this.client.requestAirdrop(publicKey, process.env.FRIENDBOT_URL);
-    }
+    const transaction = await this.client.prepareTransaction(
+      txBuilder.build(),
+      process.env.PASSPHRASE
+    );
 
-    async sendTransaction(
-        contractId: string,
-        method: string,
-        signer: Keypair,
-        ...args: xdr.ScVal[]
-    ): Promise<SorobanRpc.GetTransactionResponse> {
-        const source = await this.client.getAccount(signer.publicKey());
-        const contract = new Contract(contractId);
+    transaction.sign(signer);
 
-        const operation = new TransactionBuilder(source, {
-            fee: "100",
-            networkPassphrase: process.env.PASSPHRASE,
-        }).addOperation(contract.call(method, ...args || []))
-            .setTimeout(TimeoutInfinite)
-            .build();
+    const response = await this.client.sendTransaction(transaction);
 
-        const transaction = await this.client.prepareTransaction(
-            operation,
-            process.env.PASSPHRASE);
+    let result: SorobanRpc.GetTransactionResponse;
+    let attempts = 10;
 
-        transaction.sign(signer);
+    do {
+      await delay(1000);
+      result = await this.client.getTransaction(response.hash);
+      attempts--;
+    } while (result.status === "NOT_FOUND" && attempts > 0);
 
-        const response = await this.client.sendTransaction(transaction);
-
-        let result: SorobanRpc.GetTransactionResponse;
-        let attempts = 10;
-
-        do {
-            await delay(1000);
-            result = await this.client.getTransaction(response.hash);
-            attempts--;
-        } while (result.status === "NOT_FOUND" && attempts > 0);
-
-        return result;
-    }
+    return result;
+  }
 }
 
-let delay = promisify((ms, res) => setTimeout(res, ms))
+let delay = promisify((ms, res) => setTimeout(res, ms));
