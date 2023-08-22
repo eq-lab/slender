e94528402eb2a0e61425470d238ed5a93102da41
Artur Abliazimov <artur.abliazimov@equilibrium.io>
2023-08-22 19:40:30 +0300

    exceeded-limit-fix

--- pool/src/storage.rs
+++ pool/src/storage.rs
@@ -16,6 +16,8 @@ pub enum DataKey {
     PriceFeed(Address),
     Pause,
     STokenUnderlyingBalance(Address),
+    TokenSupply(Address),           // exceeded-limit-fix (S/Debt Token Address)
+    TokenBalance(Address, Address), // exceeded-limit-fix (S/Debt/Underlying Token Address, Account Address)
 }
 
 pub fn has_admin(env: &Env) -> bool {
@@ -157,4 +159,49 @@ pub fn add_stoken_underlying_balance(
     write_stoken_underlying_balance(env, s_token_address, total_supply)?;
 
     Ok(total_supply)
+}
+
+#[cfg(feature = "exceeded-limit-fix")]
+pub fn add_token_total_supply(env: &Env, token: &Address, amount: i128) -> Result<i128, Error> {
+    let mut total_supply = read_token_total_supply(env, token);
+
+    total_supply = total_supply
+        .checked_add(amount)
+        .ok_or(Error::MathOverflowError)?;
+
+    let data_key = DataKey::TokenSupply(token.clone());
+    env.storage().instance().set(&data_key, &total_supply);
+
+    Ok(total_supply)
+}
+
+#[cfg(feature = "exceeded-limit-fix")]
+pub fn read_token_total_supply(env: &Env, token: &Address) -> i128 {
+    let data_key = DataKey::TokenSupply(token.clone());
+    env.storage().instance().get(&data_key).unwrap_or(0i128)
+}
+
+#[cfg(feature = "exceeded-limit-fix")]
+pub fn add_token_balance(
+    env: &Env,
+    token: &Address,
+    account: &Address,
+    amount: i128,
+) -> Result<i128, Error> {
+    let mut total_supply = read_token_balance(env, token, account);
+
+    total_supply = total_supply
+        .checked_add(amount)
+        .ok_or(Error::MathOverflowError)?;
+
+    let data_key = DataKey::TokenBalance(token.clone(), account.clone());
+    env.storage().instance().set(&data_key, &total_supply);
+
+    Ok(total_supply)
+}
+
+#[cfg(feature = "exceeded-limit-fix")]
+pub fn read_token_balance(env: &Env, token: &Address, account: &Address) -> i128 {
+    let data_key = DataKey::TokenBalance(token.clone(), account.clone());
+    env.storage().instance().get(&data_key).unwrap_or(0i128)
 }
