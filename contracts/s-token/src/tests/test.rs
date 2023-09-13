0ecaf5b85e85deed8491e73aeee4a1319d7321a9
Artur Abliazimov <artur.abliazimov@equilibrium.io>
2023-09-13 18:35:32 +0300

    Fix tests

--- contracts/s-token/src/tests/test.rs
+++ contracts/s-token/src/tests/test.rs
@@ -102,7 +102,6 @@ fn test() {
     let init_reserve_input = InitReserveInput {
         s_token_address: s_token.address.clone(),
         debt_token_address: debt_token.address.clone(),
-        decimals: 9,
     };
     pool.init_reserve(
         &underlying.address,
