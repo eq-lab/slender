b0914c712fc41ac8bc7ab317744d0c5f184df638
Artur Abliazimov <artur.abliazimov@equilibrium.io>
2023-09-26 15:18:24 +0300

    Add instance storage bumps

--- contracts/common-token/src/storage.rs
+++ contracts/common-token/src/storage.rs
@@ -2,9 +2,13 @@ use soroban_sdk::{contracttype, Address, Env, String};
 use soroban_token_sdk::metadata::TokenMetadata;
 use soroban_token_sdk::TokenUtils;
 
-pub(crate) const DAY_IN_LEDGERS: u32 = 17280;
+pub(crate) const DAY_IN_LEDGERS: u32 = 17_280;
+
 pub(crate) const LOW_USER_DATA_BUMP_LEDGERS: u32 = 10 * DAY_IN_LEDGERS; // 20 days
 pub(crate) const HIGH_USER_DATA_BUMP_LEDGERS: u32 = 20 * DAY_IN_LEDGERS; // 30 days
+
+pub const LOW_INSTANCE_BUMP_LEDGERS: u32 = 1 * DAY_IN_LEDGERS; // 1 day
+pub const HIGH_INSTANCE_BUMP_LEDGERS: u32 = 7 * DAY_IN_LEDGERS; // 7 days
 
 #[derive(Clone)]
 #[contracttype]
@@ -15,27 +19,39 @@ pub enum CommonDataKey {
     TotalSupply,
 }
 
-pub fn read_pool(e: &Env) -> Address {
-    e.storage()
+pub fn read_pool(env: &Env) -> Address {
+    env.storage()
+        .instance()
+        .bump(LOW_INSTANCE_BUMP_LEDGERS, HIGH_INSTANCE_BUMP_LEDGERS);
+
+    env.storage()
         .instance()
         .get(&CommonDataKey::Pool)
         .expect("has pool")
 }
 
-pub fn write_pool(e: &Env, id: &Address) {
-    e.storage().instance().set(&CommonDataKey::Pool, id);
+pub fn write_pool(env: &Env, id: &Address) {
+    env.storage()
+        .instance()
+        .bump(LOW_INSTANCE_BUMP_LEDGERS, HIGH_INSTANCE_BUMP_LEDGERS);
+
+    env.storage().instance().set(&CommonDataKey::Pool, id);
 }
 
-pub fn has_pool(e: &Env) -> bool {
-    e.storage().instance().has(&CommonDataKey::Pool)
+pub fn has_pool(env: &Env) -> bool {
+    env.storage()
+        .instance()
+        .bump(LOW_INSTANCE_BUMP_LEDGERS, HIGH_INSTANCE_BUMP_LEDGERS);
+
+    env.storage().instance().has(&CommonDataKey::Pool)
 }
 
-pub fn read_balance(e: &Env, addr: Address) -> i128 {
+pub fn read_balance(env: &Env, addr: Address) -> i128 {
     let key = CommonDataKey::Balance(addr);
-    let balance = e.storage().persistent().get(&key);
+    let balance = env.storage().persistent().get(&key);
 
     if balance.is_some() {
-        e.storage().persistent().bump(
+        env.storage().persistent().bump(
             &key,
             LOW_USER_DATA_BUMP_LEDGERS,
             HIGH_USER_DATA_BUMP_LEDGERS,
@@ -45,22 +61,22 @@ pub fn read_balance(e: &Env, addr: Address) -> i128 {
     balance.unwrap_or(0)
 }
 
-pub fn write_balance(e: &Env, addr: Address, amount: i128) {
+pub fn write_balance(env: &Env, addr: Address, amount: i128) {
     let key = CommonDataKey::Balance(addr);
-    e.storage().persistent().set(&key, &amount);
-    e.storage().persistent().bump(
+    env.storage().persistent().set(&key, &amount);
+    env.storage().persistent().bump(
         &key,
         LOW_USER_DATA_BUMP_LEDGERS,
         HIGH_USER_DATA_BUMP_LEDGERS,
     );
 }
 
-pub fn is_authorized(e: &Env, addr: Address) -> bool {
+pub fn is_authorized(env: &Env, addr: Address) -> bool {
     let key = CommonDataKey::State(addr);
-    let is_authorized = e.storage().persistent().get(&key);
+    let is_authorized = env.storage().persistent().get(&key);
 
     if is_authorized.is_some() {
-        e.storage().persistent().bump(
+        env.storage().persistent().bump(
             &key,
             LOW_USER_DATA_BUMP_LEDGERS,
             HIGH_USER_DATA_BUMP_LEDGERS,
@@ -70,45 +86,53 @@ pub fn is_authorized(e: &Env, addr: Address) -> bool {
     is_authorized.unwrap_or(true)
 }
 
-pub fn write_authorization(e: &Env, addr: Address, is_authorized: bool) {
+pub fn write_authorization(env: &Env, addr: Address, is_authorized: bool) {
     let key = CommonDataKey::State(addr);
-    e.storage().persistent().set(&key, &is_authorized);
-    e.storage().persistent().bump(
+    env.storage().persistent().set(&key, &is_authorized);
+    env.storage().persistent().bump(
         &key,
         LOW_USER_DATA_BUMP_LEDGERS,
         HIGH_USER_DATA_BUMP_LEDGERS,
     );
 }
 
-pub fn read_decimal(e: &Env) -> u32 {
-    let util = TokenUtils::new(e);
+pub fn read_decimal(env: &Env) -> u32 {
+    let util = TokenUtils::new(env);
     util.metadata().get_metadata().decimal
 }
 
-pub fn read_name(e: &Env) -> String {
-    let util = TokenUtils::new(e);
+pub fn read_name(env: &Env) -> String {
+    let util = TokenUtils::new(env);
     util.metadata().get_metadata().name
 }
 
-pub fn read_symbol(e: &Env) -> String {
-    let util = TokenUtils::new(e);
+pub fn read_symbol(env: &Env) -> String {
+    let util = TokenUtils::new(env);
     util.metadata().get_metadata().symbol
 }
 
-pub fn read_total_supply(e: &Env) -> i128 {
-    e.storage()
+pub fn read_total_supply(env: &Env) -> i128 {
+    env.storage()
+        .instance()
+        .bump(LOW_INSTANCE_BUMP_LEDGERS, HIGH_INSTANCE_BUMP_LEDGERS);
+
+    env.storage()
         .instance()
         .get(&CommonDataKey::TotalSupply)
         .unwrap_or(0)
 }
 
-pub fn write_total_supply(e: &Env, val: i128) {
-    e.storage()
+pub fn write_total_supply(env: &Env, val: i128) {
+    env.storage()
+        .instance()
+        .bump(LOW_INSTANCE_BUMP_LEDGERS, HIGH_INSTANCE_BUMP_LEDGERS);
+
+    env.storage()
         .instance()
         .set(&CommonDataKey::TotalSupply, &val);
 }
 
-pub fn write_metadata(e: &Env, metadata: TokenMetadata) {
-    let util = TokenUtils::new(e);
+pub fn write_metadata(env: &Env, metadata: TokenMetadata) {
+    let util = TokenUtils::new(env);
     util.metadata().set_metadata(&metadata);
 }
