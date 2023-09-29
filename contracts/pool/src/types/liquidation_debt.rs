073c3e0c342cdcaf8bea963a6762eaff727ba60f
Maks Nabokov <max.nab@equilibrium.io>
2023-09-29 17:58:00 +0300

    merge fix

--- contracts/pool/src/types/liquidation_debt.rs
+++ contracts/pool/src/types/liquidation_debt.rs
@@ -6,7 +6,6 @@ pub struct LiquidationDebt {
     pub asset: Address,
     pub reserve_data: ReserveData,
     pub debt_token_balance: i128,
-    pub asset_price: i128,
     pub debt_coeff: i128,
     pub compounded_debt: i128,
 }
