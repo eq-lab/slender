d5d18d0005c180391be2663ffec496cf7af771b7
Artur Abliazimov <artur.abliazimov@equilibrium.io>
2023-10-03 15:31:15 +0300

    Release

--- integration-tests/tests/pool/6.tx.budget.spec.ts
+++ integration-tests/tests/pool/6.tx.budget.spec.ts
@@ -141,7 +141,7 @@ describe("LendingPool: methods must not exceed CPU/MEM limits", function () {
     // TODO: requires optimization
     it("Case 6: flash_loan", async function () {
         const flashLoanReceiverMock = await deployFlashLoanReceiverMock();
-        await initializeFlashLoanReceiver(client, adminKeys, flashLoanReceiverMock);
+        await initializeFlashLoanReceiver(client, adminKeys, flashLoanReceiverMock, false);
 
         const loanAssets: FlashLoanAsset[] = [
             {
