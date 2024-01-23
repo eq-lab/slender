71b32766ff26b36f398dcba147b349cb14f96f16
Artur Abliazimov <artur.abliazimov@equilibrium.io>
2024-01-23 18:12:51 +0300

    Uncomment tests

--- contracts/pool/src/tests/account_position.rs
+++ contracts/pool/src/tests/account_position.rs
@@ -1,95 +1,93 @@
-// TODO: uncomment
+use super::sut::fill_pool_three;
+use crate::tests::sut::init_pool;
+use crate::*;
+use price_feed_interface::types::asset::Asset;
+use price_feed_interface::types::price_data::PriceData;
+use soroban_sdk::testutils::Address as _;
+use soroban_sdk::vec;
 
-// use super::sut::fill_pool_three;
-// use crate::tests::sut::init_pool;
-// use crate::*;
-// use price_feed_interface::types::asset::Asset;
-// use price_feed_interface::types::price_data::PriceData;
-// use soroban_sdk::testutils::Address as _;
-// use soroban_sdk::vec;
+#[test]
+#[should_panic(expected = "HostError: Error(Contract, #202)")]
+fn should_fail_when_user_config_not_exist() {
+    let env = Env::default();
+    env.mock_all_auths();
 
-// #[test]
-// #[should_panic(expected = "HostError: Error(Contract, #202)")]
-// fn should_fail_when_user_config_not_exist() {
-//     let env = Env::default();
-//     env.mock_all_auths();
+    let sut = init_pool(&env, false);
+    let (_, _, liquidator, _) = fill_pool_three(&env, &sut);
 
-//     let sut = init_pool(&env, false);
-//     let (_, _, liquidator, _) = fill_pool_three(&env, &sut);
+    sut.pool.account_position(&liquidator);
+}
 
-//     sut.pool.account_position(&liquidator);
-// }
+#[test]
+fn should_update_when_deposit_borrow_withdraw_liquidate_price_change() {
+    let env = Env::default();
+    env.mock_all_auths();
 
-// #[test]
-// fn should_update_when_deposit_borrow_withdraw_liquidate_price_change() {
-//     let env = Env::default();
-//     env.mock_all_auths();
+    let sut = init_pool(&env, false);
 
-//     let sut = init_pool(&env, false);
+    let debt_token = sut.reserves[1].token.address.clone();
+    let deposit_token = sut.reserves[0].token.address.clone();
 
-//     let debt_token = sut.reserves[1].token.address.clone();
-//     let deposit_token = sut.reserves[0].token.address.clone();
+    let lender = Address::generate(&env);
+    let borrower = Address::generate(&env);
 
-//     let lender = Address::generate(&env);
-//     let borrower = Address::generate(&env);
+    for i in 0..3 {
+        let amount = (i == 0).then(|| 10_000_000).unwrap_or(1_000_000_000);
 
-//     for i in 0..3 {
-//         let amount = (i == 0).then(|| 10_000_000).unwrap_or(1_000_000_000);
+        sut.reserves[i].token_admin.mint(&lender, &amount);
+        sut.reserves[i].token_admin.mint(&borrower, &amount);
+    }
 
-//         sut.reserves[i].token_admin.mint(&lender, &amount);
-//         sut.reserves[i].token_admin.mint(&borrower, &amount);
-//     }
+    for i in 0..3 {
+        let amount = (i == 0).then(|| 1_000_000).unwrap_or(100_000_000);
 
-//     for i in 0..3 {
-//         let amount = (i == 0).then(|| 1_000_000).unwrap_or(100_000_000);
+        sut.pool
+            .deposit(&lender, &sut.reserves[i].token.address, &amount);
+    }
 
-//         sut.pool
-//             .deposit(&lender, &sut.reserves[i].token.address, &amount);
-//     }
+    sut.pool.deposit(&borrower, &deposit_token, &1_000_000); // 100_000_000
+    let position_after_deposit = sut.pool.account_position(&borrower);
 
-//     sut.pool.deposit(&borrower, &deposit_token, &1_000_000); // 100_000_000
-//     let position_after_deposit = sut.pool.account_position(&borrower);
+    sut.pool.borrow(&borrower, &debt_token, &40_000_000);
+    let position_after_borrow = sut.pool.account_position(&borrower);
 
-//     sut.pool.borrow(&borrower, &debt_token, &40_000_000);
-//     let position_after_borrow = sut.pool.account_position(&borrower);
+    sut.pool
+        .withdraw(&borrower, &deposit_token, &100_000, &lender);
+    let position_after_withdraw = sut.pool.account_position(&borrower);
 
-//     sut.pool
-//         .withdraw(&borrower, &deposit_token, &100_000, &lender);
-//     let position_after_withdraw = sut.pool.account_position(&borrower);
+    sut.price_feed.init(
+        &Asset::Stellar(debt_token.clone()),
+        &vec![
+            &env,
+            PriceData {
+                price: 14_000_000_000_000_000,
+                timestamp: 0,
+            },
+        ],
+    );
 
-//     sut.price_feed.init(
-//         &Asset::Stellar(debt_token.clone()),
-//         &vec![
-//             &env,
-//             PriceData {
-//                 price: 14_000_000_000_000_000,
-//                 timestamp: 0,
-//             },
-//         ],
-//     );
+    let position_after_change_price = sut.pool.account_position(&borrower);
 
-//     let position_after_change_price = sut.pool.account_position(&borrower);
+    sut.pool.liquidate(&lender, &borrower, &false);
+    let position_after_liquidate = sut.pool.account_position(&borrower);
 
-//     sut.pool.liquidate(&lender, &borrower, &debt_token, &false);
-//     let position_after_liquidate = sut.pool.account_position(&borrower);
+    assert_eq!(position_after_deposit.discounted_collateral, 600_000);
+    assert_eq!(position_after_deposit.debt, 0);
+    assert_eq!(position_after_deposit.npv, 600_000);
 
-//     assert_eq!(position_after_deposit.discounted_collateral, 600_000);
-//     assert_eq!(position_after_deposit.debt, 0);
-//     assert_eq!(position_after_deposit.npv, 600_000);
+    assert_eq!(position_after_borrow.discounted_collateral, 600_000);
+    assert_eq!(position_after_borrow.debt, 400_000);
+    assert_eq!(position_after_borrow.npv, 200_000);
 
-//     assert_eq!(position_after_borrow.discounted_collateral, 600_000);
-//     assert_eq!(position_after_borrow.debt, 400_000);
-//     assert_eq!(position_after_borrow.npv, 200_000);
+    assert_eq!(position_after_withdraw.discounted_collateral, 540_000);
+    assert_eq!(position_after_withdraw.debt, 400_000);
+    assert_eq!(position_after_withdraw.npv, 140_000);
 
-//     assert_eq!(position_after_withdraw.discounted_collateral, 540_000);
-//     assert_eq!(position_after_withdraw.debt, 400_000);
-//     assert_eq!(position_after_withdraw.npv, 140_000);
+    assert_eq!(position_after_change_price.discounted_collateral, 540_000);
+    assert_eq!(position_after_change_price.debt, 560_000);
+    assert_eq!(position_after_change_price.npv, -20_000);
 
-//     assert_eq!(position_after_change_price.discounted_collateral, 540_000);
-//     assert_eq!(position_after_change_price.debt, 560_000);
-//     assert_eq!(position_after_change_price.npv, -20_000);
-
-//     assert_eq!(position_after_liquidate.discounted_collateral, 170_400);
-//     assert_eq!(position_after_liquidate.debt, 0);
-//     assert_eq!(position_after_liquidate.npv, 170_400);
-// }
+    assert_eq!(position_after_liquidate.discounted_collateral, 170_400);
+    assert_eq!(position_after_liquidate.debt, 0);
+    assert_eq!(position_after_liquidate.npv, 170_400);
+}
--- contracts/pool/src/tests/collat_coeff.rs
+++ contracts/pool/src/tests/collat_coeff.rs
@@ -1,143 +1,141 @@
-// TODO: uncomment
+use crate::tests::sut::{fill_pool_three, init_pool, DAY};
+use crate::*;
+use price_feed_interface::types::asset::Asset;
+use price_feed_interface::types::price_data::PriceData;
+use soroban_sdk::testutils::{Address as _, Ledger};
+use soroban_sdk::vec;
 
-// use crate::tests::sut::{fill_pool_three, init_pool, DAY};
-// use crate::*;
-// use price_feed_interface::types::asset::Asset;
-// use price_feed_interface::types::price_data::PriceData;
-// use soroban_sdk::testutils::{Address as _, Ledger};
-// use soroban_sdk::vec;
+#[test]
+fn should_update_when_deposit_borrow_withdraw_liquidate() {
+    let env = Env::default();
+    env.mock_all_auths();
 
-// #[test]
-// fn should_update_when_deposit_borrow_withdraw_liquidate() {
-//     let env = Env::default();
-//     env.mock_all_auths();
+    let sut = init_pool(&env, false);
 
-//     let sut = init_pool(&env, false);
+    let debt_token = sut.reserves[1].token.address.clone();
+    let deposit_token = sut.reserves[0].token.address.clone();
 
-//     let debt_token = sut.reserves[1].token.address.clone();
-//     let deposit_token = sut.reserves[0].token.address.clone();
+    let lender = Address::generate(&env);
+    let borrower = Address::generate(&env);
 
-//     let lender = Address::generate(&env);
-//     let borrower = Address::generate(&env);
+    for i in 0..3 {
+        let amount = (i == 0).then(|| 10_000_000).unwrap_or(1_000_000_000);
 
-//     for i in 0..3 {
-//         let amount = (i == 0).then(|| 10_000_000).unwrap_or(1_000_000_000);
+        sut.reserves[i].token_admin.mint(&lender, &amount);
+        sut.reserves[i].token_admin.mint(&borrower, &amount);
+    }
 
-//         sut.reserves[i].token_admin.mint(&lender, &amount);
-//         sut.reserves[i].token_admin.mint(&borrower, &amount);
-//     }
+    env.ledger().with_mut(|l| l.timestamp = DAY);
 
-//     env.ledger().with_mut(|l| l.timestamp = DAY);
+    for i in 0..3 {
+        let amount = (i == 0).then(|| 1_000_000).unwrap_or(100_000_000);
 
-//     for i in 0..3 {
-//         let amount = (i == 0).then(|| 1_000_000).unwrap_or(100_000_000);
+        sut.pool
+            .deposit(&lender, &sut.reserves[i].token.address, &amount);
+    }
 
-//         sut.pool
-//             .deposit(&lender, &sut.reserves[i].token.address, &amount);
-//     }
+    sut.pool.deposit(&borrower, &deposit_token, &1_000_000);
+    sut.pool.borrow(&borrower, &debt_token, &40_000_000);
 
-//     sut.pool.deposit(&borrower, &deposit_token, &1_000_000);
-//     sut.pool.borrow(&borrower, &debt_token, &40_000_000);
+    env.ledger().with_mut(|l| l.timestamp = 2 * DAY);
 
-//     env.ledger().with_mut(|l| l.timestamp = 2 * DAY);
+    let collat_coeff_initial = sut.pool.collat_coeff(&debt_token);
 
-//     let collat_coeff_initial = sut.pool.collat_coeff(&debt_token);
+    sut.pool
+        .withdraw(&borrower, &deposit_token, &100_000, &lender);
 
-//     sut.pool
-//         .withdraw(&borrower, &deposit_token, &100_000, &lender);
+    env.ledger().with_mut(|l| l.timestamp = 3 * DAY);
+    let collat_coeff_after_withdraw = sut.pool.collat_coeff(&debt_token);
 
-//     env.ledger().with_mut(|l| l.timestamp = 3 * DAY);
-//     let collat_coeff_after_withdraw = sut.pool.collat_coeff(&debt_token);
+    sut.pool.borrow(&borrower, &debt_token, &10_000_000);
 
-//     sut.pool.borrow(&borrower, &debt_token, &10_000_000);
+    env.ledger().with_mut(|l| l.timestamp = 4 * DAY);
+    let collat_coeff_after_borrow = sut.pool.collat_coeff(&debt_token);
 
-//     env.ledger().with_mut(|l| l.timestamp = 4 * DAY);
-//     let collat_coeff_after_borrow = sut.pool.collat_coeff(&debt_token);
+    sut.price_feed.init(
+        &Asset::Stellar(debt_token.clone()),
+        &vec![
+            &env,
+            PriceData {
+                price: 12_000_000_000_000_000,
+                timestamp: 0,
+            },
+        ],
+    );
 
-//     sut.price_feed.init(
-//         &Asset::Stellar(debt_token.clone()),
-//         &vec![
-//             &env,
-//             PriceData {
-//                 price: 12_000_000_000_000_000,
-//                 timestamp: 0,
-//             },
-//         ],
-//     );
+    env.ledger().with_mut(|l| l.timestamp = 5 * DAY);
+    let collat_coeff_after_price_change = sut.pool.collat_coeff(&debt_token);
 
-//     env.ledger().with_mut(|l| l.timestamp = 5 * DAY);
-//     let collat_coeff_after_price_change = sut.pool.collat_coeff(&debt_token);
+    sut.pool.liquidate(&lender, &borrower, &false);
 
-//     sut.pool.liquidate(&lender, &borrower, &debt_token, &false);
+    env.ledger().with_mut(|l| l.timestamp = 6 * DAY);
+    let collat_coeff_after_liquidate = sut.pool.collat_coeff(&debt_token);
 
-//     env.ledger().with_mut(|l| l.timestamp = 6 * DAY);
-//     let collat_coeff_after_liquidate = sut.pool.collat_coeff(&debt_token);
+    assert_eq!(collat_coeff_initial, 1_000_000_010);
+    assert_eq!(collat_coeff_after_withdraw, 1_000_000_010);
+    assert_eq!(collat_coeff_after_borrow, 1_000_199_500);
+    assert_eq!(collat_coeff_after_price_change, 1_000_266_010);
+    assert_eq!(collat_coeff_after_liquidate, 1_000_295_560);
+}
 
-//     assert_eq!(collat_coeff_initial, 1_000_000_010);
-//     assert_eq!(collat_coeff_after_withdraw, 1_000_000_010);
-//     assert_eq!(collat_coeff_after_borrow, 1_000_199_500);
-//     assert_eq!(collat_coeff_after_price_change, 1_000_266_010);
-//     assert_eq!(collat_coeff_after_liquidate, 1_000_295_560);
-// }
+#[test]
+fn should_change_over_time() {
+    let env = Env::default();
+    env.mock_all_auths();
 
-// #[test]
-// fn should_change_over_time() {
-//     let env = Env::default();
-//     env.mock_all_auths();
+    let sut = init_pool(&env, false);
+    let (_, _, _, debt_config) = fill_pool_three(&env, &sut);
+    let debt_token = debt_config.token.address.clone();
 
-//     let sut = init_pool(&env, false);
-//     let (_, _, _, debt_config) = fill_pool_three(&env, &sut);
-//     let debt_token = debt_config.token.address.clone();
+    let collat_coeff_1 = sut.pool.collat_coeff(&debt_token);
 
-//     let collat_coeff_1 = sut.pool.collat_coeff(&debt_token);
+    env.ledger().with_mut(|l| l.timestamp = 4 * DAY);
+    let collat_coeff_2 = sut.pool.collat_coeff(&debt_token);
 
-//     env.ledger().with_mut(|l| l.timestamp = 4 * DAY);
-//     let collat_coeff_2 = sut.pool.collat_coeff(&debt_token);
+    env.ledger().with_mut(|l| l.timestamp = 5 * DAY);
+    let collat_coeff_3 = sut.pool.collat_coeff(&debt_token);
 
-//     env.ledger().with_mut(|l| l.timestamp = 5 * DAY);
-//     let collat_coeff_3 = sut.pool.collat_coeff(&debt_token);
+    assert_eq!(collat_coeff_1, 1_000_330_700);
+    assert_eq!(collat_coeff_2, 1_000_440_960);
+    assert_eq!(collat_coeff_3, 1_000_551_220);
+}
 
-//     assert_eq!(collat_coeff_1, 1_000_330_700);
-//     assert_eq!(collat_coeff_2, 1_000_440_960);
-//     assert_eq!(collat_coeff_3, 1_000_551_220);
-// }
+#[test]
+fn should_change_when_elapsed_time_gte_window() {
+    let env = Env::default();
+    env.mock_all_auths();
 
-// #[test]
-// fn should_change_when_elapsed_time_gte_window() {
-//     let env = Env::default();
-//     env.mock_all_auths();
+    let sut = init_pool(&env, false);
+    sut.pool.set_reserve_timestamp_window(&20);
 
-//     let sut = init_pool(&env, false);
-//     sut.pool.set_reserve_timestamp_window(&20);
+    let (_, _, _, debt_config) = fill_pool_three(&env, &sut);
+    let debt_token = debt_config.token.address.clone();
 
-//     let (_, _, _, debt_config) = fill_pool_three(&env, &sut);
-//     let debt_token = debt_config.token.address.clone();
+    let collat_coeff_1 = sut.pool.collat_coeff(&debt_token);
 
-//     let collat_coeff_1 = sut.pool.collat_coeff(&debt_token);
+    env.ledger().with_mut(|li| li.timestamp = 3 * DAY + 19);
 
-//     env.ledger().with_mut(|li| li.timestamp = 3 * DAY + 19);
+    let collat_coeff_2 = sut.pool.collat_coeff(&debt_token);
 
-//     let collat_coeff_2 = sut.pool.collat_coeff(&debt_token);
+    env.ledger().with_mut(|li| li.timestamp = 3 * DAY + 26);
 
-//     env.ledger().with_mut(|li| li.timestamp = 3 * DAY + 26);
+    let collat_coeff_3 = sut.pool.collat_coeff(&debt_token);
 
-//     let collat_coeff_3 = sut.pool.collat_coeff(&debt_token);
+    env.ledger().with_mut(|li| li.timestamp = 3 * DAY + 51);
 
-//     env.ledger().with_mut(|li| li.timestamp = 3 * DAY + 51);
+    let collat_coeff_4 = sut.pool.collat_coeff(&debt_token);
 
-//     let collat_coeff_4 = sut.pool.collat_coeff(&debt_token);
+    env.ledger().with_mut(|li| li.timestamp = 3 * DAY + 55);
 
-//     env.ledger().with_mut(|li| li.timestamp = 3 * DAY + 55);
+    let collat_coeff_5 = sut.pool.collat_coeff(&debt_token);
 
-//     let collat_coeff_5 = sut.pool.collat_coeff(&debt_token);
+    env.ledger().with_mut(|li| li.timestamp = 3 * DAY + 61);
 
-//     env.ledger().with_mut(|li| li.timestamp = 3 * DAY + 61);
+    let collat_coeff_6 = sut.pool.collat_coeff(&debt_token);
 
-//     let collat_coeff_6 = sut.pool.collat_coeff(&debt_token);
-
-//     assert_eq!(collat_coeff_1, collat_coeff_2);
-//     assert!(collat_coeff_3 > collat_coeff_2);
-//     assert!(collat_coeff_4 > collat_coeff_3);
-//     assert_eq!(collat_coeff_4, collat_coeff_5);
-//     assert!(collat_coeff_6 > collat_coeff_5);
-// }
+    assert_eq!(collat_coeff_1, collat_coeff_2);
+    assert!(collat_coeff_3 > collat_coeff_2);
+    assert!(collat_coeff_4 > collat_coeff_3);
+    assert_eq!(collat_coeff_4, collat_coeff_5);
+    assert!(collat_coeff_6 > collat_coeff_5);
+}
