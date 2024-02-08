3b8bda3f7ec6205c40e996628b6991f73c66e3d6
Maks Nabokov <max.nab@equilibrium.io>
2024-02-08 09:59:06 +0300

    renames

--- interfaces/pool-interface/src/types/collateral_params_input.rs
+++ interfaces/pool-interface/src/types/collateral_params_input.rs
@@ -5,8 +5,9 @@ use soroban_sdk::contracttype;
 #[derive(Clone, Copy)]
 pub struct CollateralParamsInput {
     /// The total amount of an asset the protocol accepts into the market.
-    pub liquidity_cap: i128,
-    pub liquidation_order: u32,
+    pub liq_cap: i128,
+    /// Liquidation order
+    pub pen_order: u32,
     pub util_cap: u32,
     /// Specifies what fraction of the underlying asset counts toward
     /// the portfolio collateral value [0%, 100%].
--- interfaces/pool-interface/src/types/reserve_configuration.rs
+++ interfaces/pool-interface/src/types/reserve_configuration.rs
@@ -6,7 +6,7 @@ pub struct ReserveConfiguration {
     pub is_active: bool,
     pub borrowing_enabled: bool,
     pub liquidity_cap: i128,
-    pub liquidation_order: u32,
+    pub pen_order: u32,
     pub util_cap: u32,
     /// Specifies what fraction of the underlying asset counts toward
     /// the portfolio collateral value [0%, 100%].
@@ -17,7 +17,7 @@ impl ReserveConfiguration {
     pub(crate) fn default() -> Self {
         Self {
             liquidity_cap: Default::default(),
-            liquidation_order: Default::default(),
+            pen_order: Default::default(),
             util_cap: Default::default(),
             is_active: true,
             borrowing_enabled: false,
