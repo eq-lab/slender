16074bd5ce78dc7fd328f4d43606b74129ac6540
Artur Abliazimov <artur.abliazimov@equilibrium.io>
2024-06-08 11:41:54 +0300

    Set twap_records: 3

--- integration-tests/tests/pool.sut.ts
+++ integration-tests/tests/pool.sut.ts
@@ -129,7 +129,7 @@ export async function init(
                     feed_asset: "XLM",
                     feed_decimals: 14,
                     feed: process.env.SLENDER_PRICE_FEED,
-                    twap_records: 1,
+                    twap_records: 3,
                     min_timestamp_delta: 100_000_000_000,
                     timestamp_precision: "Sec"
                 }]
@@ -145,7 +145,7 @@ export async function init(
                     feed_asset: "XRP",
                     feed_decimals: 16,
                     feed: process.env.SLENDER_PRICE_FEED,
-                    twap_records: 1,
+                    twap_records: 3,
                     min_timestamp_delta: 100_000_000_000,
                     timestamp_precision: "Sec"
                 }]
@@ -161,7 +161,7 @@ export async function init(
                     feed_asset: "USDC",
                     feed_decimals: 16,
                     feed: process.env.SLENDER_PRICE_FEED,
-                    twap_records: 1,
+                    twap_records: 3,
                     min_timestamp_delta: 100_000_000_000,
                     timestamp_precision: "Sec"
                 }]
@@ -177,7 +177,7 @@ export async function init(
                     feed_asset: "RWA",
                     feed_decimals: 16,
                     feed: process.env.SLENDER_PRICE_FEED,
-                    twap_records: 1,
+                    twap_records: 3,
                     min_timestamp_delta: 100_000_000_000,
                     timestamp_precision: "Sec"
                 }]
